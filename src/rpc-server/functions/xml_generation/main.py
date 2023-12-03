98694fbb1a8e03b3a3860075a8199cad95223b0c
Plopes99 <plopes99.pl@gmail.com>
2023-12-03 19:56:20 +0000

    TP1 conclusion
    improvements in menu navigation add back and exit options
    
    Exiting the program will clear the database
    
    minor corrections

--- src/rpc-server/functions/xml_generation/main.py
+++
@@ -1,6 +0,0 @@
-from functions.csv_to_xml_converter import CSVtoXMLConverter
-
-if __name__ == "__main__":
-    converter = CSVtoXMLConverter("../data/aviation_accidents.csv")
-    print(converter.to_xml_str())
-
